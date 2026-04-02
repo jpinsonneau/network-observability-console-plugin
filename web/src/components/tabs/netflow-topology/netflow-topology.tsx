ddc7272cd025ada74473f2056a7e7142d2e81ec8
Julien Pinsonneau <jpinsonn@redhat.com>
2026-04-02 19:05:06 +0200

    css fixes for glass contrast

--- web/src/components/tabs/netflow-topology/netflow-topology.tsx
+++ web/src/components/tabs/netflow-topology/netflow-topology.tsx
@@ -247,7 +247,7 @@ export const NetflowTopology = React.forwardRef<NetflowTopologyHandle, NetflowTo
               left: 0,
               right: 0,
               bottom: 0,
-              backgroundColor: props.isDark ? '#151515' : '#ffffff',
+              backgroundColor: 'var(--pf-t--global--background--color--primary--default)',
               zIndex: 1000
             }}
           >
