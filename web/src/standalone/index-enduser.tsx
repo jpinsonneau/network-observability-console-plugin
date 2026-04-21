09e24933692c86cb8d782a3b3965bdfdc75d8447
Julien Pinsonneau <jpinsonn@redhat.com>
2026-04-21 17:35:32 +0200

    fix standalone build

--- web/src/standalone/index-enduser.tsx
+++ web/src/standalone/index-enduser.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './app';
 import { initI18N } from './init-i18n';
 
 initI18N().then(() => {
-  const root = createRoot(document.getElementById('app')!);
-  root.render(<App endUser={true} />);
+  //render App component in div
+  ReactDOM.render(<App endUser={true} />, document.getElementById('app'));
 });
